d5fe396958382601ccfd891304468760acf4b320
radiusmedia <michael@radiusmedia.com.au>
2017-10-25 17:54:22 +1100

    Adding program files

---
+++ ContinuousIntegration/ContinuousIntegration/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ContinuousIntegration
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            var p = new MainClass();
+
+            string returned = p.AddNewline("This is a test");
+            Console.WriteLine(returned);
+
+            int result = p.MultiplyInput(5, 5);
+            Console.WriteLine(result);
+
+        }
+
+        //Function to replace space in string with new line
+		public string AddNewline(string input)
+		{
+			string[] data = input.Split();
+			string output = "";
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				output += data[i] + "\n";
+			}
+			return output;
+		}
+
+        //Function to multiply two integer inputs
+        public int MultiplyInput(int x, int y)
+        {
+            return (x * y);
+        }
+
+    }
+}
